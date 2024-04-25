47e5cd86b24a0e71903b61026e9ae82377860ce7
Francois Lacoursiere <lacouf@gmail.com>
2024-04-25 07:49:00 -0400

    Tests with Spring Boot Security

---
+++ src/main/java/ca/cal/mockmvctest/SpringBootMockMvcTestApplication.java
@@ -0,0 +1,13 @@
+package ca.cal.mockmvctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMockMvcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMockMvcTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/cal/mockmvctest/SpringBootMockMvcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.cal.mockmvctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMockMvcTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
